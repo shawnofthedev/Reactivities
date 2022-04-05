3faf4df0cf646dbfbf8efa816fc3a116dd2b21e0
Shawn_Votaw <Shawn_Votaw@nwtc.com>
2022-04-05 15:38:34 -0400

    End of section 7

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -4,11 +4,12 @@ import '@progress/kendo-theme-default/dist/all.css';
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
+import { store, StoreContext } from './app/stores/store';
 
 ReactDOM.render(
-  // <React.StrictMode>
-    <App />,
-  // </React.StrictMode>,
+  <StoreContext.Provider value={store}>
+    <App />
+  </StoreContext.Provider>,
   document.getElementById('root')
 );
 
