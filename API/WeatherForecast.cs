546ea70c189e9baf76cf902ffe65591f816627a0
Shawn_Votaw <Shawn_Votaw@nwtc.com>
2022-04-26 21:54:55 -0400

    Finish section 12

--- API/WeatherForecast.cs
+++ API/WeatherForecast.cs
@@ -1,14 +1,14 @@
-using System;
+// using System;
 
-namespace API;
+// namespace API;
 
-public class WeatherForecast
-{
-    public DateTime Date { get; set; }
+// public class WeatherForecast
+// {
+//     public DateTime Date { get; set; }
 
-    public int TemperatureC { get; set; }
+//     public int TemperatureC { get; set; }
 
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-    public string Summary { get; set; }
-}
+//     public string Summary { get; set; }
+// }
